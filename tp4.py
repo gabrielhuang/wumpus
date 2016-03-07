a3901c607a7e904c0dfbf376ad417bd408bce89e
Gabriel HUANG <gabriel.huang@student.ecp.fr>
2016-03-08 00:06:04 +0100

    add softmax, greedy, and nonworking ucb

--- tp4.py
+++ tp4.py
@@ -8,6 +8,7 @@ Note that actions start at 1, resulting in some awkward code.
 '''
 
 import numpy as np
+import scipy.stats
 from wumpus_text import Agent, Action
 
 # Convert moving direction to flashlight direction
@@ -60,8 +61,8 @@ def ravel(coords, dims):
     Flatten indices using C-style order
     '''
     id = coords[0]
-    for s, dim in zip(coords[1:], dims[:-1]):
-        id += s
+    for s, dim in zip(coords[1:], dims[1:]):
+        id = id * dim + s
     return id
 
 
@@ -87,7 +88,7 @@ class StateEncoding(object):
 
 class BSF(StateEncoding):
     def __init__(self, n_flash):
-        StateEncoding.__init__(self, [2, 2, n_flash])
+        StateEncoding.__init__(self, [2, 2, n_flash+1])
 
     def get_state(self, agent):
         return agent.state_[2:]
@@ -95,15 +96,16 @@ class BSF(StateEncoding):
 
 class ABSF(StateEncoding):
     def __init__(self, n_flash):
-        StateEncoding.__init__(self, [len(Action), 2, 2, n_flash])
+        StateEncoding.__init__(self, [len(Action), 2, 2, n_flash+1])
 
     def get_state(self, agent):
         return [agent.last_action-1] + agent.state_[2:]
+        #return [0] + agent.state_[2:]
 
 
 class XYBSF(StateEncoding):
     def __init__(self, grid_size, n_flash):
-        StateEncoding.__init__(self, [grid_size, grid_size, 2, 2, n_flash])
+        StateEncoding.__init__(self, [grid_size, grid_size, 2, 2, n_flash+1])
 
     def get_state(self, agent):
         return agent.state_
@@ -125,9 +127,20 @@ class EpsilonGreedy(Agent):
         Agent.__init__(self)
 
     def reset(self):
+        # do not actually reset training stuff
         self.first_visit = True # First visit of episode
+        self.current_action = Agent.getAction(self)  # pick random fake previous action
 
     def getAction(self):
+        '''
+        !!!DO NOT OVERLOAD THIS FUNCTION!!!
+
+        Return action and memorize in self.current_action
+        '''
+        self.current_action = self.getActionReal()
+        return self.current_action
+
+    def getActionReal(self):
         if np.random.uniform() < self.epsilon:
             # random action
             action = Agent.getAction(self)
@@ -136,19 +149,66 @@ class EpsilonGreedy(Agent):
             state_id = self.encoding.get_state_id(self)
             q_s = self.cum_rewards[state_id] / self.n_visits[state_id]
             action = Action(1 + np.argmax(q_s))
-        self.last_action = action
+        # save current action for use in nextState
         return action
 
     def nextState(self, s, reward):
         # update Q
         if not self.first_visit:
             state_id = self.encoding.get_state_id(self)
-            self.cum_rewards[state_id, self.last_action-1] += reward
-            self.n_visits[state_id, self.last_action-1] += 1.
+            self.cum_rewards[state_id, self.current_action-1] += reward
+            self.n_visits[state_id, self.current_action-1] += 1.
         else:
             self.first_visit = False
         # update internal state
         Agent.nextState(self, s, reward)
+        self.last_action = self.current_action  # current action is now last action
 
 
+
+def softmax(u):
+    '''
+    Return exp(u) / exp(u).flatten()
+    '''
+    v = np.exp(u - u.max())
+    return v / v.sum()
+
+
+class Softmax(EpsilonGreedy):
+    def __init__(self, temperature, encoding):
+        self.temperature = temperature
+        self.encoding = encoding
+        self.cum_rewards = np.zeros((np.prod(encoding.state_dims), len(Action)))  # q[s, a]
+        self.n_visits = 1. * np.ones_like(self.cum_rewards)  # number of visits, 1 by default
+        Agent.__init__(self)
+
+    def getActionReal(self):
+        # sample
+        state_id = self.encoding.get_state_id(self)
+        q_s = self.cum_rewards[state_id] / self.n_visits[state_id]
+        dist = softmax(q_s / self.temperature)
+        rvs = scipy.stats.rv_discrete(name='cust', values=(range(len(Action)), dist))
+        action = Action(1 + rvs.rvs())
+        return action
+
+
+class UCB(EpsilonGreedy):
+    def __init__(self, lbda, encoding):
+        self.lbda = lbda
+        self.encoding = encoding
+        self.cum_rewards = np.zeros((np.prod(encoding.state_dims), len(Action)))  # q[s, a]
+        self.n_visits = 0.001 * np.ones_like(self.cum_rewards)  # number of visits, 1 by default
+        Agent.__init__(self)
+
+    def getActionReal(self):
+        # sample
+        state_id = self.encoding.get_state_id(self)
+        q_s = self.cum_rewards[state_id] / self.n_visits[state_id]
+        scores = q_s + self.lbda * np.sqrt(2 *
+                    np.log(self.n_visits[state_id].sum())/self.n_visits[state_id])
+        action = Action(1 + np.argmax(scores))
+        return action
+
+    def nextState(self, s, reward):
+        EpsilonGreedy.nextState(self, s, reward)
 
